801484a96df0b1404d4d0c2d013e0bf339fc9cc3
RyutoOzawa <k021g1163@g.neec.ac.jp>
2023-01-29 01:23:20 +0900

    基盤の処理を別クラスに移行完了

--- MyGame.h
+++ MyGame.h
@@ -1,15 +1,6 @@
 #pragma once
 #include"KEngineFramework.h"
-#include"WindowsAPI.h"
-#include"DirectX.h"
-#include"Input.h"
-#include"Texture.h"
-#include"SpriteManager.h"
-#include"Sprite.h"
-#include"Object3d.h"
-#include"Camera.h"
-#include"ImguiManager.h"
-#include"AudioManager.h"
+
 #include"Player.h"
 #include"Map.h"
 #include"MagnetBlock.h"
@@ -34,39 +25,31 @@ public://メンバ関数
 	//描画
 	void Draw()override;
 
-	
-
 public://メンバ関数
 
-	//システム基盤の変数まとめ
-
-	WindowsAPI* windowsAPI = nullptr;
-	ReDirectX* directX = nullptr;
-	Input* input = nullptr;
-	SpriteManager* spriteManager = nullptr;
-	ImguiManager* imguiManager = nullptr;
-	bool endRequest = false;
 
 	//////////////////////////////////////////////
 
+	ImguiManager* imguiManager = nullptr;	//imgui用
+
 	//ゲーム内で使用する変数まとめ
-	uint32_t magnetTextureN = 0;
-	uint32_t magnetTextureS = 0;
-	uint32_t groundTexture = 0;
-	uint32_t playerTexture = 0;
+	uint32_t magnetTextureN = 0;	//N極のテクスチャ
+	uint32_t magnetTextureS = 0;	//S極のテクスチャ
+	uint32_t groundTexture = 0;		//地面のテクスチャ
+	uint32_t playerTexture = 0;		//プレイヤーのテクスチャ
 
-	Object3d blockObj[10][10][10];
+	Object3d blockObj[10][10][10];	//ブロック(マップのオブジェクト)
+	
+	Player* player = nullptr;		//プレイヤー
 
-	Player* player = nullptr;
+	std::vector<MagnetBlock> magnetBlocks;	//磁石オブジェクト
+	std::vector<MagnetData> magnetDatas;	//磁石データ格納用
 
-	std::vector<MagnetBlock> magnetBlocks;
-	std::vector<MagnetData> magnetDatas;
+	Colision* colision = nullptr;			//当たり判定管理クラス
 
-	Colision* colision = nullptr;
+	Camera camera{};						//カメラ
 
-	Camera camera{};
-
-	Map* map_ = nullptr;
+	Map* map_ = nullptr;					//マップ情報
 
 };
 
--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
+	//フレームワークを基底クラスとしたゲームクラスを生成
 	KEngineFramework* game = new MyGame();
 	
+	//ゲームクラスの処理実行
 	game->Run();
 
+	//ゲームクラス開放
 	delete game;
 
 	return 0;
